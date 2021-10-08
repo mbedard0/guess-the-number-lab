1e728a6e55c49c24edb2b4c36e4c01e174f57e66
Meiling Bedard <meiling.bedard@gmail.com>
2021-10-08 15:53:28 -0400

    add getGuess prompt

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
+  getGuess: function() {
+    prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`)
+  },
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
