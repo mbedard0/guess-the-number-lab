76b3cf40a3ce2861d0dd7fb85420615ed16a85ec
Meiling Bedard <meiling.bedard@gmail.com>
2021-10-09 19:30:01 -0400

    add render method and call it in game.play. call game.play() at end.

--- app.js
+++ app.js
@@ -15,9 +15,23 @@ const game = {
     let playerAnswer = this.getGuess();
     while (playerAnswer !== this.secretNum) {
       this.prevGuesses.push(playerAnswer);
+      this.render(playerAnswer);
       playerAnswer = this.getGuess()
     }
     let numGuesses = this.prevGuesses.length;
     alert(`Congrats! You guessed the number in ${numGuesses} tries.`)
+  },
+  render: function(guess) {
+    if (isNaN(guess)) {
+      alert(`That's not a number!`);
+    } else if (guess < 1 || guess > 100) {
+      alert(`That number is not within range.`);
+    } else if (guess < this.secretNum) {
+      alert(`Your guess was too low. Previous guesses were: ${this.prevGuesses.join(', ')}.`);
+    } else if (guess > this.secretNum) {
+      alert(`Your guess was too high. Previous guesses were: ${this.prevGuesses.join(', ')}.`);
+    }
   }
 }
+
+game.play()
