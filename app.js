624b7ae97563a4cd0aab7411c66cfc87210575ee
Meiling Bedard <meiling.bedard@gmail.com>
2021-10-09 19:25:13 -0400

    return play function into code.

--- app.js
+++ app.js
@@ -8,5 +8,9 @@ const game = {
     let answer = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`, `Type your guess here.`);
     answer = parseInt(answer);
     return answer
-  }
+  },
+  play: function () {
+    this.secretNum = Math.floor(Math.random() *
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+  },
 }
